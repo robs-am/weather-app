aec961f33d29f0ed5a5d4d6d47b816dd6688e8d3
Roberta Amaro <roberta.amaro89@gmail.com>
2022-04-25 22:08:05 -0300

    add js enter para escolha da cidade

--- script.js
+++ script.js
@@ -31,8 +31,7 @@ let weather = {
       document.querySelector(".wind").innerText =
         "Wind speed: " + speed + " km/h";
       document.querySelector(".weather").classList.remove("loading");
-      document.body.style.backgroundImage =
-        "url('https://source.unsplash.com/1600x900/?" + name + "')";
+      document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + name + "')";
     },
     search: function () {
       this.fetchWeather(document.querySelector(".searchbar").value);

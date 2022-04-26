0e53989b0e3812898905ad18387cbd190b5e5e0a
Roberta Amaro <roberta.amaro89@gmail.com>
2022-04-26 00:08:11 -0300

    add background em css novo

--- script.js
+++ script.js
@@ -21,17 +21,17 @@ let weather = {
       const { icon, description } = data.weather[0];
       const { temp, humidity } = data.main;
       const { speed } = data.wind;
-      document.querySelector(".city").innerText = "Previsão para " + name;
+      document.querySelector(".city").innerText = "Weather in " + name;
       document.querySelector(".icon").src =
         "https://openweathermap.org/img/wn/" + icon + "@2x.png";
       document.querySelector(".description").innerText = description;
       document.querySelector (".temp").innerText = (Math.trunc)(temp) + "°C";
       document.querySelector(".humidity").innerText =
-        "Umidade: " + humidity + "%";
+        "Humidity: " + humidity + "%";
       document.querySelector(".wind").innerText =
         "Wind speed: " + speed + " km/h";
       document.querySelector(".weather").classList.remove("loading");
-      document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + name + "')";
+      document.body.style.backgroundImage ="url('https://source.unsplash.com/1600x900/?" + city + "')";
     },
     search: function () {
       this.fetchWeather(document.querySelector(".searchbar").value);

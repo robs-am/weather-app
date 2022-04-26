2061d6b1e6ed6dd6660540f073a7436122b9fb18
Roberta Amaro <roberta.amaro89@gmail.com>
2022-04-25 21:33:03 -0300

    add organização da description com flexbox

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ let weather = {
       const { icon, description } = data.weather[0];
       const { temp, humidity } = data.main;
       const { speed } = data.wind;
-      document.querySelector(".city").innerText = "Previsão em " + name;
+      document.querySelector(".city").innerText = "Previsão para " + name;
       document.querySelector(".icon").src =
         "https://openweathermap.org/img/wn/" + icon + "@2x.png";
       document.querySelector(".description").innerText = description;
